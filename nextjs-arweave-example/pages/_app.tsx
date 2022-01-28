1e5920d07c7f2a80377e433de204e8ec494d15cc
jsvanilla <sisopsky@gmail.com>
2022-01-27 23:50:29 -0600

    FIXES ejemplo

--- nextjs-arweave-example/pages/_app.tsx
+++ nextjs-arweave-example/pages/_app.tsx
@@ -4,20 +4,25 @@ import { ArjsProvider, useArjs } from 'arjs-react'
 
 function _App() {
   const  wallet  = useArjs();
-  /*console.log("wallet")
-  console.log(wallet)
-  console.log(wallet.smartweave.write())*/
   const permission = { permissions: ["SIGN_TRANSACTION"] }
-  const [key, setKey] = useState('')
+  const [textData, setKey] = useState('')
 
-const activate = (connector, key) => wallet.connect(connector, key)
-const getKey = (e) =>{ setKey(e.target.value)}
+const activate = (connector:any, key:any) => wallet.connect(connector, key)
+const getTextData = (e:any) => { console.log(textData); setKey(e.target.value);}
 
-const activateWalletAndSendData = async (connector, key) => {
-  console.log("funciona")
+const postData = async () => {
+  let key = await wallet.getArweave().wallets.generate();
+  
   // PASO 1 Almacenar la transaccion en una variable 
+  let transaction1 = await wallet.transaction({
+    data: textData
+  }, key) 
 
   // PASO 2 Firmar la transacción (una vez firmada no debe modificarse, de lo contrario no podrá subirse a la permaweb)
+  await wallet.sign(transaction1)
+
+  // PASO 3 SUBE LA DATA A LA PERMAWEB
+  wallet.submit(transaction1).then((response:any)=>console.log(response))
 
 }
 
@@ -26,23 +31,6 @@ const [address, setAddress] = useState("Requesting...");
 
 wallet.ready(() => {
   if(wallet.status == "connected")(async () => {
-
-    let transaction1 = await  wallet.transaction({
-      data: '<html><head><meta charset="UTF-8"><title> Almacenamiento de data en la permaweb con NextJS y Arweave </title></head><body></body></html>'
-    }, key)  //.then(transaction => console.log(transaction))
-    console.log("wallet")
-    console.log(wallet)
-    console.log(transaction1)
-    console.log(await wallet.sign(transaction1)) // undefined
-
-    //console.log(await wallet.post(transaction1)) // sube la data a la permaweb
-    console.log(await wallet.poll(transaction1))
-    console.log(await wallet.submit(transaction1))
-   /*transaction1.post().then((response) => {
-      console.log("resultado de enviar data ..." )
-      console.log(response)
-    })*/
-
     console.log(wallet.getArweave())
     setBalance(wallet.getArweave().ar.winstonToAr( await wallet.getBalance("self")))
     setAddress(await wallet.getAddress())
@@ -66,8 +54,8 @@ return(
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-24" onClick={() => activate('arconnect', permission)}>Conecta tu wallet de Arconnect para subir datos</button>
         )   
     } 
-      <input className="py-24 shadow appearance-none border rounded w-full px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={key} placeholder={'Mensaje de texto a guardar'} onChange={getKey}/>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-24" onClick={() => activateWalletAndSendData()}>Subir data a la permaweb</button>
+      <input className="py-24 shadow appearance-none border rounded w-full px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={textData} placeholder={'Mensaje de texto a guardar'} onChange={getTextData}/>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-24" onClick={() => postData()}>Subir data a la permaweb</button>
   </div>
 </div>
 )}
